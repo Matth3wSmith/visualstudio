4cbfabff417ab24b3f09244c3c489d22a591dac4
Kovács Máté <kovacsm22@ady-nagyatad.sulinet.hu>
2024-09-11 14:20:22 +0200

    C# változók

---
+++ Valtozo/Valtozo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Valtozo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int egeszSzam = 3;
+            float lebegoPontos;
+            double d1, d2;
+            
+            Console.WriteLine(egeszSzam+10);
+            egeszSzam += 10;
+            egeszSzam /= 5;
+            Console.WriteLine(egeszSzam);
+
+
+            lebegoPontos = 12;
+            lebegoPontos /= 5;
+            Console.WriteLine("Lebegő "+lebegoPontos);
+
+            d1 = 1;
+            d2 = 2;
+            d1 *= 1.1;
+            Console.WriteLine(d1);
+
+            decimal decimalis = 12;
+            Console.WriteLine(decimalis);
+
+            string szo = "Kekcske káposzta";
+            Console.WriteLine(szo);
+            char karakter = 'a';
+            karakter++;
+            Console.WriteLine(karakter);
+
+            
+        }
+    }
+}
