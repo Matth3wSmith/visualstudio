3a127c1f74987e3064b6f95179b789ffc2f91e36
Kovács Máté <kovacsm22@ady-nagyatad.sulinet.hu>
2024-10-10 12:54:22 +0200

    Array | Házi: Kiíratás sorszámmal együtt & Egymás mellé kiíratás && Kettő darab szám bekérése, 2D tömb készítése, annak eleminek feltöltése

---
+++ Array/Array-10-10/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Array_10_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bekért számokkal tömb feltöltés
+            int tombHossz = 6;
+            int[] tomb1 = new int[tombHossz];
+            for (int i = 0; i < tombHossz; i++)
+            {
+                Console.Write("Kérek egy számot:");
+                tomb1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i=0;i<tomb1.Length;i++)
+            {
+                Console.WriteLine(tomb1[i]);
+            }
+
+            //Sorszámot is jelenítse meg a tomb kiirasnal
+        }
+    }
+}
