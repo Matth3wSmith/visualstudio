95e17cfc19331e69d060201d5ce0b0ca4a44192a
Kovács Máté <kovacsm22@ady-nagyatad.sulinet.hu>
2024-10-15 08:46:10 +0200

    Függvények 10-15

---
+++ Functions/Functions-10-15/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Functions_10_15
+{
+    internal class Program
+    {
+        static int osszeadas(int szam1, int szam2)
+        {
+            return szam1 + szam2;
+        }
+        static double osszeadas(double szam1, double szam2)
+        {
+            return szam1 + szam2;
+        }
+
+        static double osztas(int szam1, int szam2)
+        {
+            return szam1 / (double)szam2;
+        }
+        static double osztas(double szam1, double szam2)
+        {
+            return szam1 / szam2;
+        }
+        //a paraméterként kapott egész számokat összeadja és kiírja a végeredményt
+        static void osszeadasKiiras(int[] szamok)
+        {
+            int osszeg = 0;
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                osszeg += szamok[i];
+            }
+            Console.WriteLine(osszeg);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(osszeadas(10,56));
+            Console.WriteLine(osszeadas(10, 56.66));
+            Console.WriteLine("----OSZTÁS---");
+            Console.WriteLine(osztas(2,1));
+            Console.WriteLine(osztas(2, 2));
+            Console.WriteLine(osztas(2, 4));
+            Console.WriteLine(osztas(2.5, 1.2));
+
+
+        }
+    }
+}
