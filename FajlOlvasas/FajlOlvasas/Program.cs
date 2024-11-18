eedc86735188759d67995c68186ad61c5acf3ebf
Máté <komate1011@gmail.com>
2024-11-18 22:03:14 +0100

    Házi feladat

--- FajlOlvasas/FajlOlvasas/Program.cs
+++ FajlOlvasas/FajlOlvasas/Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Random random = new Random();   
-            StreamWriter ir = new StreamWriter("proba.txt");
-            for (int i = 0; i < 100; i++)
+            Random random = new Random();
+            //StreamWriter ir = new StreamWriter("proba.txt");
+            /*for (int i = 0; i < 100; i++)
             {
                 ir.Write(Convert.ToString(random.Next()) + "\t");
                 if (i % 10 == 9)
@@ -15,9 +15,43 @@
                 }
             }
 
-            ir.Close();
+            ir.Close();*/
 
             //Házi feladat: "kérjetek be kettő számot, rátok van bízva hogy mekkorát és egy akkora táblázatszerű véletlen számokat tartalmazó dolgot, elválasztó karakter legyenek pontosvessző, fájl kiterjesztése pedig .csv, sor végén ne legyen felesleges pontosvessző"
+ 
+
+            StreamWriter ir = new StreamWriter("hazi.csv");
+            Console.WriteLine("Add meg a táblázat sorainak és oszlopainak számát!");
+            int sor = Convert.ToInt32(Console.ReadLine());
+            int oszlop = Convert.ToInt32(Console.ReadLine());
+            int[] sorokHossza = new int[sor];
+            int[,] szamok = new int[sor, oszlop];
+            ir.Write("+");
+            for (int k = 0; k< oszlop; k++)
+            {
+                ir.Write("-");
+            }
+            ir.Write("+\n");
+            for (int i = 0; i < sor; i++)
+            {
+                sorokHossza[i] = oszlop-1;
+                for (int j = 0; j < oszlop; j++)
+                {
+                    szamok[i, j] = random.Next(10, 200);
+                    sorokHossza[i] += Convert.ToString(szamok[i, j]).Length;
+                    Console.WriteLine(szamok[i, j]);
+                    ir.Write(Convert.ToString(szamok[i, j]));
+                    if (j % oszlop != 9)
+                    {
+                        ir.Write(';');
+                    }
+
+                }
+                ir.Write("\n");
+
+            }
+            ir.Close();
+            Console.ReadKey();
         }
     }
 }
--- FajlOlvasas/FajlOlvasas/obj/Release/net8.0/FajlOlvasas.AssemblyInfo.cs
+++ FajlOlvasas/FajlOlvasas/obj/Release/net8.0/FajlOlvasas.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FajlOlvasas")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5ef833975ec2b570044f1ebe30d891b385f1fe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FajlOlvasas")]
