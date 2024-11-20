0b2ffffe523d34313939f76da3ca41011be2e65e
Kovács Máté <kovacsm22@ady-nagyatad.sulinet.hu>
2024-11-20 14:20:46 +0100

    Érettségi feladatok - Cb-rádió

---
+++ Erettsegi-feladatok/cbradio/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace cbradio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] adatok = File.ReadAllLines("cb.txt");
+
+            int[,] idok = new int[adatok.Length-1,2];
+            int[] adasok = new int[adatok.Length-1];
+            string[] nevek = new string[adatok.Length-1];
+
+            for (int i = 1; i < adatok.Length; i++)
+            {
+                string[] vag = adatok[i].Split(";");
+                idok[i-1,0]=int.Parse(vag[0]);
+                idok[i,1]=int.Parse(vag[1]);
+
+
+            }
+
+        }
+    }
+}
