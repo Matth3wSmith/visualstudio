3a0422d09a3afa52e6e32899032b424d0d9e4987
Kovács Máté <kovacsm22@ady-nagyatad.sulinet.hu>
2024-12-03 08:45:51 +0100

    Dictionary

---
+++ Gyakrolas/Dictionary-12-03/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Dictionary_12_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,string> dict = new Dictionary<string,string>();
+
+            dict.Add("Ádám","Cigány");
+            dict.Add("Zolika","Vroom-Vroom");
+            dict.Add("Bazsi", "PHP SQL kiráj!");
+
+            Console.WriteLine(dict["Ádám"]);
+        }
+    }
+}
