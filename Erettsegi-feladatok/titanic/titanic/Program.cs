31a5b43b7f9d088ac9272897ca465ab61669ffec
Kovács Máté <kovacsm22@ady-nagyatad.sulinet.hu>
2024-12-13 09:30:46 +0100

    Titanic érettségi feladat

---
+++ Erettsegi-feladatok/titanic/titanic/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel.Design;
+
+namespace titanic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //1. feladat
+            StreamReader olvas = new StreamReader("titanic.txt");
+            string[] adatok = olvas.ReadToEnd().Trim().Split("\n");
+            Utas[] utasok = new Utas[adatok.Length];
+            for (int i = 0; i < utasok.Length; i++)
+            {
+                utasok[i] = new Utas(adatok[i]);
+            }
+
+            olvas.Close();
+
+            //2 .feladat
+            Console.WriteLine($"2. feladat: {utasok.Length} db");
+
+            //3. feladat
+            int osszUtas = 0;
+            for (int i = 0; i < utasok.Length; i++)
+            {
+                osszUtas += utasok[i].eltunt + utasok[i].tulelo;  
+            }
+            Console.WriteLine($"3. feladat: {osszUtas} fő");
+
+            //4. feladat
+            Console.Write("4. feladat: Kulcsszó: ");
+            string kulcsszo = Console.ReadLine();
+            string talalatI = "";
+            bool vanTalalat = false;
+            for (int i = 0; i < utasok.Length; i++) { 
+
+                if (utasok[i].kategoria.Contains(kulcsszo))
+                {
+                    //Console.WriteLine(utasok[i].kategoria);
+                    talalatI += i+" ";
+                    vanTalalat = true;
+                }
+            }
+            if (!vanTalalat)
+            {
+                Console.WriteLine("\tNincs találat!");
+            }
+            else { 
+                Console.WriteLine("\tVan találat!");
+            }
+
+            //5.feladat
+            Console.WriteLine("5. feladat:");
+            if (vanTalalat) 
+            {
+                string[] talalatokI = talalatI.Split(" ");
+                for (int i = 0; i < talalatokI.Length - 1; i++)
+                {
+                    int m = Convert.ToInt32(talalatokI[i]);
+                    Console.WriteLine($"\t{utasok[m].kategoria} {utasok[m].eltunt + utasok[m].tulelo} fő");
+                }
+            }
+
+            //6. feladat
+
+
+        }
+    }
+}
---
+++ Erettsegi-feladatok/titanic/titanic/Utas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace titanic
+{
+    internal class Utas
+    {
+        public string kategoria;
+        public int tulelo;
+        public int eltunt;
+        public int arany;
+
+        public Utas(string sor)
+        {
+            //"ferfiak-masodosztaly;14;154" 
+            string[] vag = sor.Split(";");
+
+            kategoria= vag[0];
+            tulelo = int.Parse(vag[1]);
+            eltunt = int.Parse(vag[2]);
+            arany = eltunt/tulelo;
+            
+        }
+
+    }
+}
