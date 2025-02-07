a64e8854ae0d1e2d8bfb23e401da74c6d7673575
Kovács Máté <kovacsm22@ady-nagyatad.sulinet.hu>
2025-02-07 09:41:51 +0100

    Reklam digikult érettségi

---
+++ Dolgozat/reklam/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace reklam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. feladat
+            List<Rendeles> rendelesek= new List<Rendeles>();
+            StreamReader olvas = new StreamReader("rendel.txt");
+
+
+
+            while (!olvas.EndOfStream)
+            {
+                rendelesek.Add(new Rendeles(olvas.ReadLine()));
+            }
+
+            olvas.Close();
+
+            //2. feladat
+            Console.WriteLine("2. feladat");
+            Console.WriteLine("A rendelések száma: "+rendelesek.Count);
+
+            //3. feladat
+            Console.WriteLine("3. feladat"); 
+            Console.Write("Kérem, adjon meg egy napot: ");
+            int nap=int.Parse(Console.ReadLine());
+            int rendelesAznap = rendelesek.Where(rendeles => rendeles.nap==nap).Count();
+            Console.WriteLine("A rendelések száma az adott napon: "+rendelesAznap);
+
+            //4. feladat
+            Console.WriteLine("4. feladat");
+            int nincsRendeles = 30-rendelesek.Where(rendeles => rendeles.varos == "NR").Select(rendeles => rendeles.nap).Distinct().Count();
+            if (nincsRendeles == 0)
+            {
+                Console.WriteLine("Minden nap volt rendelés a reklámban nem érintett városból");
+            }
+            else
+            {
+                Console.WriteLine(nincsRendeles+" nap nem volt a reklámban nem érintett városból rendelés");
+            }
+
+
+        }
+    }
+}
---
+++ Dolgozat/reklam/Rendeles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reklam
+{
+    internal class Rendeles
+    {
+        public int nap;
+        public string varos;
+        public int darab;
+        public Rendeles(string sor)
+        {
+            string[] vag = sor.Split();
+
+            nap = int.Parse(vag[0]);
+            varos = vag[1];
+            darab = int.Parse(vag[2]);
+        }
+    }
+}
