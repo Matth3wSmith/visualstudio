b443a82b0df9a12b6565708db5de3b823527a740
Kovács Máté <kovacsm22@ady-nagyatad.sulinet.hu>
2025-03-07 08:57:35 +0100

    Sliders

---
+++ 0307-slider/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _0307_slider
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 0307-slider/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _0307_slider
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                int szam = int.Parse(szamErtek.Text);
+                sliders.Value = szam;
+            }
+            catch (Exception hiba)
+            {
+                //hibaBox.Text = hiba.ToString();
+            }
+        }
+
+        private void sliders_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            szamErtek.Text = sliders.Value.ToString();
+        }
+    }
+}
