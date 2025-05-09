4625906e003fb4efe2f2004f2215bb665baaa83a
Kovács Máté <kovacsm22@ady-nagyatad.sulinet.hu>
2025-05-09 13:12:38 +0200

    balkezesek kész

--- balkezesek/MainWindow.xaml.cs
+++ balkezesek/MainWindow.xaml.cs
@@ -38,21 +38,53 @@ namespace balkezesek
             }
             olvas.Close();
 
-            //3. feladat
         }
 
 
+        //3. feladat
         private void feladat3(object sender, RoutedEventArgs e)
         {
             harmadikFeladat.Text = Convert.ToString(jatekosok.Count());
         }
 
+        //4. feladat
         private void feladat4(object sender, RoutedEventArgs e)
         {
-            List<string> jatekosok1999utan = new List<string>();
-
+            List<string> jatekosok1999utan = jatekosok.Where(x => x.utolsoDatum.Year == 1999 && x.utolsoDatum.Month == 10).Select( x => x.nev+" "+(Math.Round(x.magassag*2.54,2))).ToList();
+            magassagok.ItemsSource = jatekosok1999utan;
 
         }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {   
+            
+        }
+
+        int bekertEvszam;
+        //5. feladat
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bekertEvszam = int.Parse(evszam.Text);
+                if (bekertEvszam < 1990 || bekertEvszam > 1999)
+                {
+                    throw new Exception();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Hibás adat, kérek egy 1990 és 1999 közötti évszámot!");
+                evszam.Text = "";
+            };
+        }
+
+        //6. feladat
+        private void atlagsuly_btn(object sender, RoutedEventArgs e)
+        {
+            double atlagSuly = Math.Round(jatekosok.Where(x => x.elsodatum.Year <= bekertEvszam && bekertEvszam <= x.utolsoDatum.Year).Select(x => x.suly).Average(), 2);
+            atlagsulyLabel.Content += ""+atlagSuly+" font";
+        }
+
     }
 
 }
